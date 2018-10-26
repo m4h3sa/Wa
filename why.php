ac72b14553bd6033abe55e39e813e7e47aa2204c
m4h3sa <44429634+m4h3sa@users.noreply.github.com>
2018-10-26 18:19:35 +0700

    Add files via upload

---
+++ why.php
@@ -0,0 +1,32 @@
+
+
+
+			 ██████╗ ██╗ ██╗███████╗███████╗
+
+			██╔════╝ ██║ ██║╚══███╔╝╚══███╔╝
+
+			██║ ███╗██║ ██║ ███╔╝ ███╔╝ 
+
+			██║ ██║██║ ██║ ███╔╝ ███╔╝ 
+
+			╚██████╔╝╚██████╔╝███████╗███████╗
+
+			 ╚═════╝ ╚═════╝ ╚══════╝╚══════╝
+
+			       HAHAHA RONI GOBLOK WKWKWKWK
+
+			  Code By : Guz Wahyu Mahesa
+				 
+				 
+			       
+			      HAHAHAHAHHAHAHAHAH KENA TIPU
+				 
+				 
+				 GAMPANG TENAN NGAPUSI RONI
+				 
+				 
+				 
+				 RONI RONDO RONCOT
+				
+				
+    
